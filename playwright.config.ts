1c6489073352008e40668fd58041554a1ff2de1c
shantanu <shanmondal92@gmail.com>
2025-06-28 19:51:34 +0530

    Updated the .yml file for headed run

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     //viewport: null,
     // launchOptions:{
     //   args : ['--start-maximized'],
