81638eb51c1dbb28f4b33662cf7202bdaf39662b
shantanu <shanmondal92@gmail.com>
2025-06-28 23:08:34 +0530

    Updated the .yml file for headless run

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'],
-    ['json', {outputFile: 'test-results/jsonreport.json'}]
-],
+  // reporter: [
+  //   ['html'],
+  //   ['json', {outputFile: 'test-results/jsonreport.json'}]
+//],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
