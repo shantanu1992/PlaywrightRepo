db1333aa93450db1e7406d858198f173f0e928f4
shantanu <shanmondal92@gmail.com>
2025-06-28 20:06:32 +0530

    Updated the .yml file for headless run

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -17,7 +17,10 @@ test('testcase desc', async ({page}) => {
     await page.fill('#username', process.env.E2E_USERNAME || '')
     await page.fill('#password', process.env.E2E_PASSWORD || '')
     await page.getByRole('button',{name:'Sign In'}).click()
-    expect(await page.locator('.navbar-brand').first().textContent()).toEqual('ProtoCommerce')
+    //expect(await page.locator('.navbar-brand').first().textContent()).toEqual('ProtoCommerce')
+    await page.screenshot({ path: 'before-navbar-check.png' });
+    await expect(page.locator('.navbar-brand').first()).toHaveText('ProtoCommerce');
+
 
     const itemToAdd = ['iphone X', 'Samsung', 'Nokia']
     for(const item of itemToAdd){
